5ff5ae06a5db9f73a5be5b8b0b7e1b6c29bc82c8
Imran <82795589+Imran2708@users.noreply.github.com>
2023-11-27 14:45:18 +0530

    Update __init__.py

--- api/getSpeechToken/__init__.py
+++ api/getSpeechToken/__init__.py
@@ -18,20 +18,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     response = requests.post(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
 
     if response.status_code == 200:
-        access_token = response.text
-
-        #store the access token in a file
-        output_file_path = os.path.join(os.environ["TMP"], "access_token.txt")
-        with open(output_file_path, "W") as output_file:
-            output_file.write(access_token)
-            
+        access_token = response.text            
         return func.HttpResponse(
              access_token,
              status_code=200
         )
     else:
-        #store the access token in a file
-        output_file_path = os.path.join(os.environ["TMP"], "error_token_code.txt")
-        with open(output_file_path, "W") as output_file:
-            output_file.write(str(response.status_code))
         return func.HttpResponse(str(response.status_code))
