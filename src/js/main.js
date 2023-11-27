4e410bf79a9b6b9f1d80f297d579ae530a966fbd
Imran <82795589+Imran2708@users.noreply.github.com>
2023-11-27 11:30:15 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -234,11 +234,26 @@ window.startSession = () => {
   speechSynthesisConfig.speechSynthesisVoiceName = TTSVoice
   document.getElementById('playVideo').className = "round-button-hide"
 
-  response ="eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJ3ZXN0dXMyIiwic3Vic2NyaXB0aW9uLWlkIjoiNTE1NzZhOGZkY2YzNDI4MDk0MDFhOGNmNmVkMmM4NjEiLCJwcm9kdWN0LWlkIjoiU3BlZWNoU2VydmljZXMuUzAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvY2RkMTMwMzAtNzdlMC00MWE1LWIwZDktNTU4YzdlMTA1NTFjL3Jlc291cmNlR3JvdXBzL0FJTUxQT0MyREVQL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29nbml0aXZlU2VydmljZXMvYWNjb3VudHMvdGV4dHRvc3BlZWNoc3R1ZGlvIiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy53ZXN0dXMyIiwiZXhwIjoxNzAxMDYzMjE0LCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMifQ.cQtrmGhtxbplZ4MD-zw8EEDqDsDwZGypNA1yyJIPRKXqvAFcG3xt7WBEQevxAP5g-J2Cj-79vY-JL4yv8yJc7g"
-  speechSynthesisConfig.authorizationToken = response;
-  token = response
-  speechSynthesizer = new SpeechSDK.SpeechSynthesizer(speechSynthesisConfig, null)
-  requestAnimationFrame(setupWebRTC)
+  try {
+    const response = await axios.post('/api/getSppechToken/__init__.py');
+    if (response.status === 200){
+      const authorizationToken = response.data;
+      speechSynthesisConfig.authorizationToken = authorizationToken;
+      token = authorizationToken;
+      speechSynthesizer = new SpeechSDK.SpeechSynthesizer(speechSynthesisConfig, null);
+      requestAnimationFrame(setupWebRTC);
+    } else {
+      console.error('Failed to get authorization token. Status:'. response.status);
+    }
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+
+  #response ="eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJ3ZXN0dXMyIiwic3Vic2NyaXB0aW9uLWlkIjoiNTE1NzZhOGZkY2YzNDI4MDk0MDFhOGNmNmVkMmM4NjEiLCJwcm9kdWN0LWlkIjoiU3BlZWNoU2VydmljZXMuUzAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvY2RkMTMwMzAtNzdlMC00MWE1LWIwZDktNTU4YzdlMTA1NTFjL3Jlc291cmNlR3JvdXBzL0FJTUxQT0MyREVQL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29nbml0aXZlU2VydmljZXMvYWNjb3VudHMvdGV4dHRvc3BlZWNoc3R1ZGlvIiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy53ZXN0dXMyIiwiZXhwIjoxNzAxMDYzMjE0LCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMifQ.cQtrmGhtxbplZ4MD-zw8EEDqDsDwZGypNA1yyJIPRKXqvAFcG3xt7WBEQevxAP5g-J2Cj-79vY-JL4yv8yJc7g"
+  #speechSynthesisConfig.authorizationToken = response;
+  #token = response
+  #speechSynthesizer = new SpeechSDK.SpeechSynthesizer(speechSynthesisConfig, null)
+  #requestAnimationFrame(setupWebRTC)
 }
 
 async function greeting() {
