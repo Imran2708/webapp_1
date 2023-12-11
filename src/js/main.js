561367ae9706e695d22201d379dec4aee0ed6ce0
Imran <82795589+Imran2708@users.noreply.github.com>
2023-12-11 14:56:40 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license.
 
+import * as SpeechSDk from "microsoft-coginitiveservices-speech-sdk";
+
 var system_prompt = `You are an AI assistant focused on delivering brief product details and assisting with the ordering process.
 - Before calling a function, aim to answer product queries using existing conversational context.
 - If the product information isn't clear or available, consult get_product_information for accurate details. Never invent answers.  
@@ -29,7 +31,7 @@ let IceServerCredential
 const BackgroundColor = '#FFFFFFFF'
 
 let token
-var SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
+//var SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
 document.addEventListener("DOMContentLoaded", function() {
   const speechSynthesisConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL("wss://{region}.tts.speech.microsoft.com/cognitiveservices/websocket/v1?enableTalkingAvatar=true".replace("{region}", CogSvcRegion)))
 });
