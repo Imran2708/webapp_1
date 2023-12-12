bffb021e0793993a34db6eb6198b2d213ab8388a
Imran <82795589+Imran2708@users.noreply.github.com>
2023-12-12 14:51:24 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,7 @@ const BackgroundColor = '#FFFFFFFF'
 let token
 //var SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
 //import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
-const SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
+//const SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
 document.addEventListener("DOMContentLoaded", function() {
   const speechSynthesisConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL("wss://{region}.tts.speech.microsoft.com/cognitiveservices/websocket/v1?enableTalkingAvatar=true".replace("{region}", CogSvcRegion)))
 });
